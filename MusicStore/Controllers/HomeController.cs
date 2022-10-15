b1fe91e90fead79d93dbf7c6f4bb4bd9492cb6b6
j-uzdowska <VetHavelock@gmail.com>
2022-10-15 13:34:38 +0200

    Dodanie widoku dla Home Index, zmiana layout i stylesheet (content)

--- MusicStore/Controllers/HomeController.cs
+++ MusicStore/Controllers/HomeController.cs
@@ -9,9 +9,9 @@ namespace MusicStore.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello from Home";
+            return View();
         }
     }
 }
