dfc0dae308e63c8a69c76613af594ac3d7881eea
j-uzdowska <VetHavelock@gmail.com>
2022-10-18 11:08:59 +0200

    Dodanie wyświetlania najlepiej sprzedających się albumów na stronie głównej

--- MusicStore/Controllers/HomeController.cs
+++ MusicStore/Controllers/HomeController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MusicStore.Models;
 
 namespace MusicStore.Controllers
 {
+    
+
     public class HomeController : Controller
     {
+        MusicStoreEntities storedB = new MusicStoreEntities();
         // GET: Home
+        
         public ActionResult Index()
         {
-            return View();
+            var albums = GetTopSellingAlbums(5);
+            return View(albums);
+        }
+
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            return storedB.Albums.OrderByDescending(a => a.OrderDetails.Count()).Take(count).ToList();
         }
     }
 }
